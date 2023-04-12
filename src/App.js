8d6710bf00062ade5f772dc82ea10f81b2ef22fe
SANJAY0115 <sanjaysanju0115@gmail.com>
2023-04-12 18:17:48 +0530

    cloud - deployed api

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
  useEffect(()=>{
     const getStudents = async () => {
      try {
-       const response = await fetch("http://localhost:1004/students", {
+       const response = await fetch("https://backend-deploy-delta.vercel.app/students", {
          method:"GET"
        }); 
        const data = await response.json();
