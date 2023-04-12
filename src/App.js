8701431eeec89dd134cdbc2dd6633713c8e09f1c
SANJAY0115 <sanjaysanju0115@gmail.com>
2023-04-12 10:37:39 +0530

    own server added

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import { useEffect, useState } from 'react';
+import "./App.css"
+import { Redirect, Route, Switch } from 'react-router-dom';
+import AuthPage from './Components/AuthPage';
+import DashBoard from './Components/DashBoard';
+import NoPage from './Components/NoPage';
+import StudentProfile from './Components/StudentProfile';
+import { StudentDetails } from './Components/Students';
+import WelcomePage from './Components/WelcomePage';
+import AddStudents from './Components/AddStudents';
+import EditStudents from './Components/EditStudents';
+
+
+function App() {
+   const [studentsData, setStudents] = useState([]);
+ // mounting - fetch data from mock api
+ useEffect(()=>{
+    const getStudents = async () => {
+     try {
+       const response = await fetch("http://localhost:1004/students", {
+         method:"GET"
+       }); 
+       const data = await response.json();
+       console.log(data);
+       setStudents(data.data)
+     } catch (error) {
+       console.log("Error Occured",error)
+     }
+    }; 
+    getStudents();
+ }, [])
+
+
+   return (
+      <div className="App">
+  
+     <Switch>
+
+     <Route exact path = "/">
+     <WelcomePage/>
+     </Route>
+
+
+      <Route path="/dashboard">
+          <DashBoard/>
+      </Route>
+
+
+      <Route path= "/register">
+        <AuthPage/>
+      </Route>
+
+      <Route path = "/details">
+        <StudentDetails 
+        studentsData={studentsData} 
+        setStudents= {setStudents}/>
+      </Route>
+
+      <Route path = "/students">
+         <Redirect to = "/details"/>
+      </Route>
+
+
+      <Route path = "/student/:id">
+        <StudentProfile studentsData={studentsData} />
+      </Route>
+
+
+      <Route path = "/add-data">
+        <AddStudents
+        studentsData={studentsData} 
+        setStudents= {setStudents}
+        />
+      </Route>
+
+      <Route path = "/edit/:id">
+        <EditStudents 
+        studentsData={studentsData}
+        setStudents= {setStudents} />
+      </Route>
+
+      <Route path = "**">
+        <NoPage/>
+      </Route>
+
+  </Switch>
+   
+
+   </div>
+ );
+}
+
+export default App;
---
+++ src/Data/data.js
@@ -0,0 +1,46 @@
+export const data =[
+    {
+        "id":"1",
+        "name":"Arun",
+        "batch":"B42WD",
+        "gender":"Male",
+        "yearsOfExperience":"3"
+    },
+    {
+        "id":"2",
+        "name":"Jayavel",
+        "batch":"B42WD",
+        "gender":"Male",
+        "yearsOfExperience":"2"
+    },
+    {
+        "id":"3",
+        "name":"Santhos",
+        "batch":"B42WD",
+        "gender":"Male",
+        "yearsOfExperience":"1"
+    },
+    {
+        "id":"4",
+        "name":"Sharmila",
+        "batch":"B42WD",
+        "gender":"Female",
+        "yearsOfExperience":"4"
+        
+    },
+    {
+        "id":"5",
+        "name":"Keerthana",
+        "batch":"B42WD",
+        "gender":"Female",
+        "yearsOfExperience":"1"
+    },
+    {
+        "id":"6",
+        "name":"Mathialagan",
+        "batch":"B42WD",
+        "gender":"Male",
+        "yearsOfExperience":"2"
+    }
+]
+
